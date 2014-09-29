4bb5a6c627247b23b959cfe47cea4e4d65ee4ff0
matt <matt@r710.venturecompound.local>
2014-09-29 17:30:59 -0400

    turn on puppetdb

---
+++ manifests/site.pp
@@ -0,0 +1,6 @@
+node 'r710.venturecompound.local' {
+  # Configure puppetdb and its underlying database
+  class { 'puppetdb': }
+  # Configure the puppet master to use puppetdb
+  class { 'puppetdb::master::config': }
+}
