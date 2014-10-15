fc743fd8e012bf5ec9fce8dc764c9740b565bc06
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2014-10-15 11:29:53 -0400

    remove, do it in foreman

--- manifests/site.pp
+++
@@ -1,7 +0,0 @@
-node 'r710.venturecompound.local' {
-  include packages
-  # Configure puppetdb and its underlying database
-  class { 'puppetdb': }
-  # Configure the puppet master to use puppetdb
-  # class { 'puppetdb::master::config': }
-}
